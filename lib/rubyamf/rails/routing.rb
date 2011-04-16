fe742a26082a4aed90d6c1095c5decd675d3c394
Stephen Augenstein <perl.programmer@gmail.com>
2011-04-16 17:35:48 -0400

    Add namespace support to rails routing hooks and add specs to cover functionality

--- lib/rubyamf/rails/routing.rb
+++ lib/rubyamf/rails/routing.rb
@@ -1,17 +1,37 @@
 module RubyAMF::Rails
   module Routing
-    def map_amf options, params=nil
-      # Extract controller and action
-      if options.is_a?(String)
-        (controller, action) = options.split("#")
+    def map_amf *args
+      # Rails 2 namespace uses ActiveSupport::OptionMerger, which adds namespace
+      # options to last hash, or passes it as an additional parameter
+      # Rails 3 stores current namespace info in @scope variable
+
+      # Process args
+      if args[0].is_a?(String)
+        (controller, action) = args[0].split("#")
+        params = args[1]
+        rails2_with_options = args[2] || {}
       else
-        controller = options[:controller]
-        action = options[:action]
-        params = options[:params]
+        controller = args[0].delete(:controller)
+        action = args[0].delete(:action)
+        params = args[0].delete(:params)
+        rails2_with_options = args[0]
       end
 
       # Convert controller name to actual controller class name
-      controller = controller.camelize(:upper)+"Controller"
+      controller = controller.camelize(:upper)+"Controller" unless controller =~ /Controller$/
+
+      # Namespace controller name if used
+      modules = if @scope && @scope[:module]
+        # Rails 3 code path
+        @scope[:module].split("/").map {|n| n.camelize(:upper)}
+      elsif rails2_with_options[:namespace]
+        # Rails 2 code path
+        namespaces = rails2_with_options[:namespace].split("/")
+        namespaces.map {|n| n.camelize(:upper)}
+      else
+        []
+      end
+      modules.reverse.each {|m| controller = "#{m}::#{controller}"}
 
       RubyAMF.configuration.map_params controller, action, params
     end
---
+++ spec/rails_routing_spec.rb
@@ -0,0 +1,55 @@
+require "spec_helper.rb"
+require "rubyamf/rails/routing"
+
+describe RubyAMF::Rails::Routing do
+  include RubyAMF::Rails::Routing
+  def test_map *args
+    config = mock(RubyAMF::Configuration)
+    config.stub!(:map_params).and_return do |c,a,p|
+      @controller = c
+      @action = a
+      @params = p
+    end
+    RubyAMF.stub!(:configuration).and_return(config)
+    map_amf *args
+  end
+
+  it "should support string style call" do
+    test_map("user#show", [:asdf, :fdsa])
+    @controller.should == "UserController"
+    @action.should == "show"
+    @params.should == [:asdf, :fdsa]
+  end
+
+  it "should support option style call" do
+    test_map(:controller => "user", :action => "show", :params => [:asdf, :fdsa])
+    @controller.should == "UserController"
+    @action.should == "show"
+    @params.should == [:asdf, :fdsa]
+  end
+
+  it "should not camelize controller name if already camelized" do
+    test_map(:controller => "AdminController", :action => "login", :params => [])
+    @controller.should == "AdminController"
+  end
+
+  describe "Rails 2" do
+    it "should recognize namespace with string style call" do
+      test_map(:controller => "user", :action => "show", :params => [], :namespace => "namespace/nested")
+      @controller.should == "Namespace::Nested::UserController"
+    end
+
+    it "should recognize namespace with option style call" do
+      test_map("user#show", [], {:namespace => "namespace/nested"})
+      @controller.should == "Namespace::Nested::UserController"
+    end
+  end
+
+  describe "Rails 3" do
+    it "should recognize namespace" do
+      @scope = {:module => "namespace/nested"}
+      test_map("user#show", [])
+      @controller.should == "Namespace::Nested::UserController"
+    end
+  end
+end
